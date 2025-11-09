b7fb9eb956570214720bf92150e8392cdd5ea597
RyosukeDTomita <d310151c2@gmail.com>
2025-11-09 21:58:53 +0900

    feat: greedy algorithm

--- src/2025-11-08/C_2.hs
+++ src/2025-11-08/C_2.hs
@@ -1,6 +1,7 @@
 -- ChatGPTにかいてもらった貪欲法バージョン(これでTLE解決)
 import Data.List (sort)
 
+-- 前提: hs、bsは昇順でソート済み
 check :: [Int] -> [Int] -> Int -> Bool
 check hs bs k = go hs bs 0
   where
@@ -10,15 +11,15 @@ check hs bs k = go hs bs 0
       | h <= b =
           if cnt + 1 == k
             then True
-            else go hs' bs' (cnt + 1)
-      | otherwise = go (h : hs') bs' cnt
+            else go hs' bs' (cnt + 1) -- カウントを増やして次の頭とからだパーツを試すため再帰
+      | otherwise = go (h : hs') bs' cnt -- 頭よりも体が軽かった場合には頭は変えずに体パーツのindexのみをずらして、より重い体パーツで再帰
 
 main :: IO ()
 main = interact $ \input ->
   let xs = map read (words input) :: [Int]
-      n = xs !! 0
-      m = xs !! 1
-      k = xs !! 2
-      hs = sort $ take n $ drop 3 xs
-      bs = sort $ take m $ drop (3 + n) xs
+      n = xs !! 0 -- 頭のパーツの数
+      m = xs !! 1 -- 体のパーツの数
+      k = xs !! 2 -- 作りたい倒れないロボットの数
+      hs = sort $ take n $ drop 3 xs -- 頭のパーツの重さリスト
+      bs = sort $ take m $ drop (3 + n) xs -- 体パーツの重さリスト
    in unlines [if check hs bs k then "Yes" else "No"]
