88d6a642552d34725a3005c1cbcae6d6554da162
RyosukeDTomita <d310151c2@gmail.com>
2026-02-10 23:32:33 +0900

    feat: list monad

--- src/templates/list/charToInt.hs
+++ src/templates/list/charToInt.hs
@@ -1,6 +1,7 @@
-import Data.Char (digitToInt)
+import Data.Char (digitToInt, intToDigit)
 
 main :: IO ()
 main = do
   print $ map digitToInt "123" -- [1,2,3] CharをIntに変換する
+  print $ map intToDigit [1, 2, 3]
   print $ toInteger 123 -- 既に数値になっているものをIntegerに変換する。Charなどは変換できないので注意
