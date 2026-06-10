f5c05b23f3eafcc6e08a3d2e7789761f8d566c1d
RyosukeDTomita <d310151c2@gmail.com>
2026-06-10 14:38:09 +0900

    feat: abc057C

---
+++ src/atcoder_practice/brute_force_search/abc057C.hs
@@ -0,0 +1,15 @@
+{-# OPTIONS_GHC -Wunused-imports #-}
+{-# LANGUAGE BangPatterns #-}
+import Control.Arrow ((>>>))
+
+solve :: Int -> Int
+solve n = f lowerDivisor (n `div` lowerDivisor)
+  where
+    lowerDivisor = maximum $ filter (\x -> n `mod` x == 0) [1 ..(floor $ sqrt (fromIntegral n:: Double))] -- 素因数分解した時に小さい側の約数で最大のものを探す。
+    f :: Int -> Int -> Int
+    f a b = length . show $ max a b
+
+main :: IO ()
+main =
+  interact $
+    (read :: String -> Int) >>> solve >>> show >>> (++ "\n")
