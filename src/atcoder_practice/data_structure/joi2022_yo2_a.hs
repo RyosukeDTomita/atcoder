edc4e434fd25cbda7ba17fbb658e516cc2db7b78
RyosukeDTomita <d310151c2@gmail.com>
2026-07-13 20:20:08 +0900

    feat: joi2022_yo2_a

---
+++ src/atcoder_practice/data_structure/joi2022_yo2_a.hs
@@ -0,0 +1,36 @@
+-- https://atcoder.jp/contests/joi2022yo2/tasks/joi2022_yo2_a
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE MonoLocalBinds #-}
+{-# OPTIONS_GHC -Wno-x-partial #-}
+{-# OPTIONS_GHC -Wunused-imports #-}
+import Debug.Trace (traceShowId)
+import Data.List (mapAccumL)
+
+-- {-# OPTIONS_GHC -DATCODER #-}
+#ifdef ATCODER
+debug :: Bool ; debug = False
+#else
+debug :: Bool ; debug = True
+#endif
+
+dbgId :: (Show a) => a -> a
+dbgId x
+  | debug = traceShowId x
+  | otherwise = x
+
+solve :: [String] -> [String]
+solve ss = filter (/="") $ snd $ mapAccumL go [] ss
+  where
+    go :: [String] -> String -> ([String], String)
+    go acc s
+      | s == "READ" = let (book : rest) = acc in (rest, book)
+      | otherwise = (s : acc, "")
+
+main :: IO ()
+main =
+  interact $ \inputs ->
+    let ls = lines inputs
+        -- q = (read :: String -> Int) $ head ls
+        ss = tail ls
+     in unlines $ solve ss
