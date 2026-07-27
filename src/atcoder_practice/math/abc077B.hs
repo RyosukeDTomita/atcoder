174ccadbe2ea615ab2228afd0bee73662a7e2e50
RyosukeDTomita <d310151c2@gmail.com>
2026-07-27 11:33:54 +0900

    feat: abc077b

---
+++ src/atcoder_practice/math/abc077B.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE MonoLocalBinds #-}
+{-# OPTIONS_GHC -Wno-x-partial #-}
+{-# OPTIONS_GHC -Wunused-imports #-}
+
+import Control.Arrow ((>>>))
+import Debug.Trace (traceShowId)
+
+-- {-# OPTIONS_GHC -DATCODER #-}
+#ifdef ATCODER
+debug :: Bool ; debug = False
+#else
+debug :: Bool ; debug = True
+#endif
+
+dbgId :: (Show a) => a -> a
+dbgId x
+  | debug = traceShowId x
+  | otherwise = x
+
+solve :: Int -> Int
+solve n =
+  let x = floor $ sqrt (fromIntegral n :: Double)
+   in x ^ 2
+
+main :: IO ()
+main =
+  interact $
+    (read :: String -> Int) >>> solve >>> show >>> (++ "\n")
