bc877b962088d19d5f778db9b94b946122c82ba2
RyosukeDTomita <d310151c2@gmail.com>
2026-08-17 08:41:20 +0900

    chore: abc471C

---
+++ src/2026-08-15/C_2.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE MonoLocalBinds #-}
+{-# OPTIONS_GHC -Wmissing-local-signatures #-}
+{-# OPTIONS_GHC -Wmissing-signatures #-}
+{-# OPTIONS_GHC -Wno-x-partial #-}
+{-# OPTIONS_GHC -Wtype-defaults #-}
+{-# OPTIONS_GHC -Wunused-imports #-}
+
+-- TLE調査時に有効化する。MRが適用されて単相化された(=共有が効いている)束縛を報告する。
+-- {-# OPTIONS_GHC -Wmonomorphism-restriction #-}
+
+import Debug.Trace (traceShowId)
+import Data.List (sort)
+
+-- {-# OPTIONS_GHC -DATCODER #-}
+#ifdef ATCODER
+debug :: Bool ; debug = False
+#else
+debug :: Bool ; debug = True
+#endif
+
+dbgId :: (Show a) => a -> a
+dbgId x
+  | debug = traceShowId x
+  | otherwise = x
+
+-- 正と負それぞれのに近い順にしたリストを作り、それぞれの先頭の候補2つを比較して近い方に進む。
+solve :: [Int] -> Int
+solve as = go (0, 0, nAs', pAs)
+  where
+    (nAs, pAs) = break (> 0) $ sort as
+    -- nAs' = sortOn (Down) nAs
+    nAs' = reverse nAs -- 100 msくらい速くなった。上でsortしてあるのを利用する。
+    go :: (Int, Int, [Int], [Int]) -> Int
+    go (_, !result, [], []) = result
+    go (ai, !result, [], (p: pRest)) = go (p, (result + abs (ai - p)), [], pRest)
+    go (ai, !result, (n: nRest), []) = go (n, (result + abs (ai - n)), nRest, [])
+    go (ai, !result, nList@(n : nRest), pList@(p: pRest)) =
+      let nDiff = abs (ai - n)
+          pDiff = abs (ai - p)
+        in if nDiff <= pDiff then go (n, result + nDiff, nRest, pList) else go (p, result + pDiff, nList, pRest)
+
+main :: IO ()
+main =
+  interact $ \inputs ->
+    let ls = lines inputs
+        n = (read :: String -> Int) $ head ls
+        as = map read . words $ ls !! 1 :: [Int]
+     in show (solve as) ++ "\n"
--- src/2026-08-15/README.md
+++ src/2026-08-15/README.md
@@ -8,4 +8,4 @@ https://atcoder.jp/contests/abc471
 
 - [A - Nine or Nein](https://atcoder.jp/contests/abc471/tasks/abc471_a): 割り算の部分で`div`を使って1ペナ。掛け算にするべし
 - [B - Survey Tabulation](https://atcoder.jp/contests/abc471/tasks/abc471_b): `group`して最大を探す。
-- [C - Cookies and Greedy Takahashi](https://atcoder.jp/contests/abc471/tasks/abc471_c): ポイントは座標iが常に変わり、そこからもっとも近い座標を探す必要があること。座標の長さ的に都度探索してもTLEしなさそうなので検索コストを抑えるために`Data.Set`を使ってiより小さい座標のうち最大のもの or iより大きい座標のうち最小のものを候補として取り出し距離が近い方に移動することを繰り返す。
+- [C - Cookies and Greedy Takahashi](https://atcoder.jp/contests/abc471/tasks/abc471_c): ポイントは座標iが常に変わり、そこからもっとも近い座標を探す必要があること。座標の長さ的に都度探索してもTLEしなさそうなので検索コストを抑えるために`Data.Set`を使ってiより小さい座標のうち最大のもの or iより大きい座標のうち最小のものを候補として取り出し距離が近い方に移動することを繰り返す。C_2.hsはあまり好みではないが、正と負のマスを分けてそれぞれ0から近い順に並べて同じようなことをするパターンも書いた。
